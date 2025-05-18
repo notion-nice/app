a1646b0a785b0aa2e16b01b386b6ef2d8a595366
SengMitnick <sengmitnick@163.com>
2025-05-18 18:40:27 +0800

    feat: 更新 next.config.mjs，动态设置 API 地址并添加新的 API 重写规则

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import { createMDX } from "fumadocs-mdx/next";
 const withMDX = createMDX();
 
+const isDev = process.env.NODE_ENV === "development";
+
+const apiUrl = isDev ? "http://43.139.161.167:8000" : "http://10.1.24.16:8000";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -11,8 +15,28 @@ const nextConfig = {
       destination: "/style-editor",
     },
     {
+      source: "/api/v1/:path*",
+      destination: `${apiUrl}/api/v1/:path*`,
+    },
+    {
+      source: "/api/admin/:path*",
+      destination: `${apiUrl}/api/admin/:path*`,
+    },
+    {
+      source: "/api/lifedev/:path*",
+      destination: `${apiUrl}/api/lifedev/:path*`,
+    },
+    {
       source: "/api/nice/:path*",
-      destination: "http://10.1.24.16:8000/api/nice/:path*",
+      destination: `${apiUrl}/api/nice/:path*`,
+    },
+    {
+      source: "/api/pay/:path*",
+      destination: `${apiUrl}/api/pay/:path*`,
+    },
+    {
+      source: "/api/mp/:path*",
+      destination: `${apiUrl}/api/mp/:path*`,
     },
   ],
 };
